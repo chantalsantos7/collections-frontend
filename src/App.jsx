2c41722692925ca657c74c46de6915658d9538a3
Chantal Santos <chantalsantos7@gmail.com>
2024-05-21 16:23:26 +0100

    add routing to auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react'
+import { createBrowserRouter, Link, Route, RouterProvider, Routes } from 'react-router-dom'
 import './fonts.css';
 import SignupPage from './components/SignupPage'
 import LoginPage from './components/LoginPage'
+import HomePage from './components/HomePage';
+
+const router = createBrowserRouter([
+  {path: "/", element: <HomePage />}, 
+  {path: "/auth/*", 
+  children:[
+    {path: "signup", element: <SignupPage /> },
+    {path: "login", element: <LoginPage />}
+  ] },
+  { path: "*", element: <Root /> }
+])
 
 
-function App() {
 
-
-  return (
-    <>
-      {/* <Header /> */}
-      {/* <LoginPage /> */}
-      <SignupPage />
-    </>
-  )
+export default function App() {
+  // return (
+  //   // <>
+  //   //   {/* <Header /> */}
+  //   //   {/* <LoginPage /> */}
+  //   //   {/* <SignupPage /> */}
+  //   // </>
+  // )
+  return <RouterProvider router={router} />
 }
 
-export default App
+function Root() {
+  return (
+      <Routes>
+
+      </Routes>
+  );
+}
