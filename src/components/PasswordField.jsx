28064f4e5fae79976e62330a80b8491049d8c65b
Chantal Santos <chantalsantos7@gmail.com>
2024-05-27 11:30:47 +0100

    feat: add input validation to signup page

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -2,14 +2,18 @@ import { useState } from "react";
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
-const PasswordField = () => {
+const PasswordField = ({ name, label, placeholder, register}) => {
 
     const [showPassword, setShowPassword] = useState(false);
 
   return (
     <>
-     <InputGroup>
-        <Form.Control type={showPassword ? "text" : "password"} />
+    <label htmlFor="" className="auth-input">{label}</label>
+     <InputGroup className="mb-3">
+        <Form.Control type={showPassword ? "text" : "password"}
+        placeholder={placeholder}
+        {...register(name, {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{6,20}$/})}
+        />
         <Button 
         onClick={() => setShowPassword(!showPassword)}>
             {showPassword ? <FaEyeSlash /> : <FaEye />}
--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -1,6 +1,5 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import EmailAndPassword from './EmailAndPassword';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
 import Header from './Header';
@@ -8,15 +7,25 @@ import './css/authpages.css';
 import Container from 'react-bootstrap/esm/Container';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
+import PasswordField from './PasswordField';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import PasswordField from './PasswordField';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+
+const validationSchema = yup.object({
+  email: yup.string().required("Email is required").email("Email is invalid"),
+  password: yup.string().min(6, 'Password must be at least 6 characters').matches().required("Password is required"),
+  confirmPassword: yup.string().oneOf([yup.ref('password')], 'Passwords must match').required("Confirm password is required")
+})
 
 const SignupPage = () => {
 
   const {
     register, handleSubmit, watch, formState: { errors },
-  } = useForm();
+  } = useForm({ resolver: yupResolver(validationSchema) });
+
 
   const onSubmit = (data) => console.log(data);
 
@@ -30,35 +39,22 @@ const SignupPage = () => {
               <Card.Title className='text-center m-3 p-1'>Sign Up</Card.Title>
               <Card.Body className='auth-card-bg'>
                 <Form onSubmit={handleSubmit(onSubmit)}>
-                  {/* <EmailAndPassword /> */}
                   <label htmlFor="" id="email" className='auth-input'>*Email address</label>
                   <InputGroup className='mb-3'>
                     <Form.Control
                       placeholder='email@example.com'
                       aria-label='Email'
-                      {...register("email", {required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/})}
+                      // {...register("email", { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })}
                     // onChange={handleEmailChange}
                     />
                   </InputGroup>
+                  <p>{errors.email?.message}</p>
 
-                  <PasswordField />
-
-                  {/* <label htmlFor="" id="password" className='auth-input'>*Password</label>
-                  <InputGroup className='mb-3'>
-                    <Form.Control
-                      placeholder='Password must be 8-20 characters long'
-                      aria-label='Password'
-                      {...register("password", {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,20}$/})}
-                    // onChange={handlePasswordChange}
-                    />
-                  </InputGroup>
-                  <label htmlFor="" id="confirm-password" className='auth-input'>*Confirm Password</label>
-                  <InputGroup className='mb-3'>
-                    <Form.Control
-                      placeholder='Enter password again'
-                      {...register("confirm_password", {required: true, pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,20}$/})}
-                      />
-                  </InputGroup> */}
+                  <PasswordField name="password" label="*Password" placeholder="Password must be 6-20 characters long" register={register} />
+                  <p>{errors.password?.message}</p>
+                  <PasswordField name="confirm-password" label="*Confirm Password" placeholder="Enter password again" register={register} />
+                  <p>{errors.confirmPassword?.message}</p>
+                  
                   <div className='d-flex justify-content-center'>
                     <Button className='auth-button' type='submit'>Create an account</Button>
                   </div>
